15f7c400a75323bdd2dcb9635be24576f15a0e24
Jared Ruiz <jared707@gmail.com>
2024-09-18 22:49:53 -0500

    Added updates for mobile queries, preparing for project information and getting reacquainted with some of the css I had initially set up for my other components. Next website project I will use a styling framework to speed up development and keep things clean.

--- client/src/Components/UX/index.js
+++ client/src/Components/UX/index.js
@@ -3,22 +3,32 @@ import React from "react";
 function UX() {
     return(
         <div className="ux">
-            <div className="ux-intro">
-                <h2 id="ux-h2-title"><span id="vapor">Google UX Design Portfolio (WIP)</span></h2>
-                <p>Welcome! This will be where I house my projects from my Google UX Design Course! I will periodically update the contents as I continue through development! Thank you for your patience! - J</p>
-            </div>
-
-            <div className="ux-proj-1">
-
+            <div className="ux-intro-block">
+                
+                <div className="test">
+                    <h2 id="ux-h2-title"><span id="vapor">Google UX Design Portfolio</span></h2>
+                    <p>Welcome! This will be where I house my projects from my Google UX Design Course! I will periodically update the contents as I continue through development! Thank you for your patience!</p>
+                    <p>-J</p>
+                </div>
+                
                 <div className="proj-1-title">
-                    <h2 id="ux-h2-title">Project 1: Museum Application</h2>
+                    <h2 id="ux-h2-title"><span id="vapor">Project: 1 (WIP)</span></h2>
+                    <p>Museum Application</p>
                 </div>
-
-                <div>
-
-                </div>
-
+            
+            
             </div>
+
+            {/* <div className="ux-proj-1">
+
+                
+                </div> 
+                    <div className="summary-1-block">
+                        <div className="proj-1-sum">
+                            <h2 id="ux-h2-title">Summary</h2>
+                    </div>
+                </div> */}
+            
 
 
 
