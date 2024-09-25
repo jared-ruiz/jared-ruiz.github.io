16412a1de1aa81c1ca9faaeadd711ed11a2ad012
Jared Ruiz <jared707@gmail.com>
2024-09-24 21:19:51 -0500

    Added more content to UI/UX section, still adjusting CSS and media queries.

--- client/src/Components/UX/index.js
+++ client/src/Components/UX/index.js
@@ -5,32 +5,54 @@ function UX() {
         <div className="ux">
             <div className="ux-intro-block">
                 
-                <div className="test">
+                <div className="ux-wip">
                     <h2 id="ux-h2-title"><span id="vapor">Google UX Design Portfolio</span></h2>
-                    <p>Welcome! This will be where I house my projects from my Google UX Design Course! I will periodically update the contents as I continue through development! Thank you for your patience!</p>
+                    <p>Welcome! This will be where I house my projects from my Google UX Design Course! I will periodically update the contents below as I continue through development! Thank you for your patience!</p>
                     <p>-J</p>
                 </div>
                 
-                <div className="proj-1-title">
+                {/* <div className="proj-1-title">
                     <h2 id="ux-h2-title"><span id="vapor">Project: 1 (WIP)</span></h2>
                     <p>Museum Application</p>
+                </div> */}
+            </div>
+            <div className="proj-1-block">
+                <div className="proj-1-pre-brief">
+                    <h2 id="remove-margin">Project 1:</h2>
+                    <h2 id="close-together"><span id="vapor">"El Puente a Otro Mundo"</span></h2>
+                     <p>I had the pleasure of leading the UX design and research efforts for the fictional musuem "El Puente a Otro Mundo", who were in need of a new front facing website/application to better provide transparency and ease of access to all of their important events and exhibits.</p>
+                    {/* <div className="proj-1-roles">
+
+                        <div className="location">
+                            <h3>Location</h3>
+                            <p>San Antonio, Texas</p>
+                        </div>
+
+                        <div className="responsibilites">
+                            <h3>Responsibilites</h3>
+                            <p>UX/UI Design</p>
+
+                        </div>
+
+                        <div className="tech">
+                            <h3>Technologies</h3>
+                            <p>Procreate, Figma, Adobe Photoshop, Adobe Illustrator, wireframe.cc</p>
+                        </div>
+
+                    </div> */}
                 </div>
+
+                {/* <div className="proj-1-research">
+                    <h2></h2>
+
+                </div> */}
+
+
+            
+            
             
             
             </div>
-
-            {/* <div className="ux-proj-1">
-
-                
-                </div> 
-                    <div className="summary-1-block">
-                        <div className="proj-1-sum">
-                            <h2 id="ux-h2-title">Summary</h2>
-                    </div>
-                </div> */}
-            
-
-
 
 
             
