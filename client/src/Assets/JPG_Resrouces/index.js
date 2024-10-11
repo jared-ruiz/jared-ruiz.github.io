c30cf000a473b04a0ff520627248a9f6cf06d035
Jared Ruiz <jared707@gmail.com>
2024-10-10 21:22:35 -0500

    Added new graphic to designs page.

--- client/src/Assets/JPG_Resrouces/index.js
+++ client/src/Assets/JPG_Resrouces/index.js
@@ -1,9 +1,10 @@
 const ImageArray = [
 
+
         {
             index: 0,
-            title: "pier_glitch",
-            src: require('../../Assets/Compressed_Images/pier_glitch-min.jpg')
+            title: "paper_teeth",
+            src: require('../../Assets/Compressed_Images/woman_coat_02-min.jpg')
         },
         {
             index: 1,
@@ -28,62 +29,67 @@ const ImageArray = [
             src: require('../../Assets/Compressed_Images/squatting_01-min.jpg')
 
         },
-
         {
             index: 5,
+            title: "pier_glitch",
+            src: require('../../Assets/Compressed_Images/pier_glitch-min.jpg')
+        },
+
+        {
+            index: 6,
             title: "pondering_hope",
             src: require('../../Assets/Compressed_Images/full_body_thinking-min.jpg')
 
         },
         {
-            index: 6,
+            index: 7,
             title: "woman_on_car",
             src: require('../../Assets/Compressed_Images/woman_on_car-min.jpg')
 
         },
         {
-            index: 7,
+            index: 8,
             title: "woman_boots",
             src: require('../../Assets/Compressed_Images/woman_boots-min.jpg')
 
         },
         {
-            index: 8,
+            index: 9,
             title: "retro_self",
             src: require('../../Assets/Compressed_Images/me_texture-min.jpg')
         },
         {
-            index: 9,
+            index: 10,
             title: "staircase_red",
             src: require('../../Assets/Compressed_Images/staircase_01-min.jpg')
         },
         {
-            index: 10,
+            index: 11,
             title: "halloween flyer",
             src: require('../../Assets/Compressed_Images/halloween_flyer_01-min.jpg')
         },
         {
-            index: 11,
+            index: 12,
             title: "shoes_ad",
             src: require('../../Assets/Compressed_Images/shoes_ad-min.jpg')
         },
         {
-            index: 12,
+            index: 13,
             title: "naja_beauty",
             src: require('../../Assets/Compressed_Images/naja_poster-min.jpg')
         },
         {
-            index: 13,
+            index: 14,
             title: "aragorn",
             src: require('../../Assets/Compressed_Images/aragorn_01-min.jpg')
         },
         {
-            index: 14,
+            index: 15,
             title: "vapor_aesth",
             src: require('../../Assets/Compressed_Images/vapor_aesth-min.jpg')
         },
         {
-            index: 15,
+            index: 16,
             title: "vintage_tech_poster",
             src: require('../../Assets/Compressed_Images/vintage-tech-poster-min.jpg')
         }
